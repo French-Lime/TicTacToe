88d488a5bafbce523321094118592cff99c5b8e4
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-04-05 22:54:13 +0300

    login/register implemented (unit of work, repository)

---
+++ TicTacToe/TicTacToe.Data/ITicTacToeData.cs
@@ -0,0 +1,15 @@
+﻿namespace TicTacToe.Data
+{
+    using TicTacToe.Data.Repositories;
+    using TicTacToe.Models;
+    using TicTacToe.Web.Models;
+
+    public interface ITicTacToeData
+    {
+        IRepository<TicTacToeUser> Users { get; }
+
+        IRepository<Game> Games { get; }
+
+        int SaveChanges();
+    }
+}
