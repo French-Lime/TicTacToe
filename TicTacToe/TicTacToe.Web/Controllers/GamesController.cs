74d498d00941f0a382dcc8115ded03ac6aec7e9f
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-04-06 15:07:06 +0300

    implemented Game.Status

--- TicTacToe/TicTacToe.Web/Controllers/GamesController.cs
+++ TicTacToe/TicTacToe.Web/Controllers/GamesController.cs
@@ -1,5 +1,6 @@
 ﻿namespace TicTacToe.Web.Controllers
 {
+    using System;
     using System.Linq;
     using System.Web.Http;
 
@@ -8,6 +9,7 @@
 
     using TicTacToe.Data;
     using TicTacToe.Models;
+    using TicTacToe.Web.DataModels;
 
     [Authorize]
     public class GamesController : ApiController
@@ -49,7 +51,42 @@
             this.data.SaveChanges();
 
             return this.Ok(game.Id);
+        }
 
+        [HttpGet]
+        public IHttpActionResult Status(string gameId)
+        {
+            var currentUserId = this.User.Identity.GetUserId();
+            var idAsGuid = new Guid(gameId);
+
+            var game = this.data.Games.All()
+                .Where(g => g.Id == idAsGuid)
+                .Select(g => new { g.FirstPlayerId, g.SecondPlayerId })
+                .FirstOrDefault();
+
+            if (game == null)
+            {
+                return this.NotFound();
+            }
+
+            if (game.FirstPlayerId != currentUserId && game.SecondPlayerId != currentUserId)
+            {
+                return this.BadRequest("This is not your game!");
+            }
+
+            var gameInfo = this.data.Games.All()
+                .Where(g => g.Id == idAsGuid)
+                .Select(g => new GameInfoDataModel()
+                {
+                    Board = g.Board,
+                    Id = g.Id,
+                    State = g.State,
+                    FirstPlayerName = g.FirstPlayer.UserName,
+                    SecondPlayerName = g.SecondPlayer.UserName,
+                })
+                .FirstOrDefault();
+
+            return this.Ok(gameInfo);
         }
     }
 }
