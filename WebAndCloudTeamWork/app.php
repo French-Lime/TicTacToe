61d479d4d65259f75fe5febc20acba0eed661d3b
Yana Slavcheva <yana.slavcheva@gmail.com>
2015-04-08 18:06:04 +0300

    Deleted in order to get renamed

--- WebAndCloudTeamWork/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (isset($_POST['eventCommand'])) {
-        if ($_POST['eventCommand'] == 'GetEventData') {
-            sleep(2);
-            echo 1234;
-        }
-    }
-?>
--- WebAndCloudTeamWork/js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    (function () {
-        var rootUrl = 'http://localhost:50511/';
-        var accountPersister = userAuth.accountPersister.get(rootUrl);
-        var accountController = userAuth.accountController.get(accountPersister);
-        accountController.load();
-    })();
-});
