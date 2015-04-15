594da5323c48787b3420856a8841a2c06bd76f89
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-04-15 21:02:51 +0300

    added ChatMessages table in DB, linked with users

---
+++ TicTacToe/TicTacToe.Models/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace TicTacToe.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using TicTacToe.Web.Models;
+
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual TicTacToeUser User { get; set; } 
+    }
+}
--- TicTacToe/TicTacToe.Models/TicTacToeUser.cs
+++ TicTacToe/TicTacToe.Models/TicTacToeUser.cs
@@ -1,27 +1,43 @@
 ﻿namespace TicTacToe.Web.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-
+    using System.Collections.Generic;
     using System.Security.Claims;
     using System.Threading.Tasks;
 
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
 
+    using TicTacToe.Models;
+
     public class TicTacToeUser : IdentityUser
     {
+        private ICollection<ChatMessage> chatMessages;
+
+        public TicTacToeUser()
+        {
+            this.chatMessages = new HashSet<ChatMessage>();
+        }
+
+        public virtual ICollection<ChatMessage> ChatMessages
+        {
+            get
+            {
+                return this.chatMessages;
+            }
+
+            set
+            {
+                this.chatMessages = value;
+            }
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<TicTacToeUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+
             // Add custom user claims here
             return userIdentity;
-        }
-
-        public int Age
-        {
-            get;
-            set;
         }
     }
 }
